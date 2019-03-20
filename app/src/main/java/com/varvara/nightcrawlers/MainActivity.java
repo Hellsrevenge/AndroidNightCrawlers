3e7276a4898a0cb187e9d382bdc3c2923aad0c6e
Hellsrevenge <ivark26@gmail.com>
2019-03-19 21:51:43 -0700

    added lyft support

--- app/src/main/java/com/varvara/nightcrawlers/MainActivity.java
+++ app/src/main/java/com/varvara/nightcrawlers/MainActivity.java
@@ -1,11 +1,14 @@
 package com.varvara.nightcrawlers;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Window;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -28,12 +31,15 @@ public class MainActivity extends AppCompatActivity {
 
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setGeolocationEnabled(true);
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
             settings.setAllowFileAccessFromFileURLs(true);
             settings.setAllowUniversalAccessFromFileURLs(true);
             settings.setDomStorageEnabled(true);
-
+//            settings.setAppCacheEnabled(true);
+//            settings.setDatabaseEnabled(true);
+            settings.setGeolocationDatabasePath(getFilesDir().getPath());
         }
 
         myWebView.setWebViewClient(new WebViewClient(){
@@ -41,6 +47,25 @@ public class MainActivity extends AppCompatActivity {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 if( url.startsWith("http:") || url.startsWith("https:") ) {
                     return false;
+                }
+
+                if( url.startsWith("lyft://") ) {
+                    boolean isInstalled = isPackageInstalled("me.lyft.android", getPackageManager());
+
+                    if (!isInstalled) {
+                        final String appPackageName = "me.lyft.android";
+                        try {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                        } catch (android.content.ActivityNotFoundException anfe) {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                        }
+                    } else {
+                        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("me.lyft.android");
+                        if (launchIntent != null) {
+                            startActivity(launchIntent);//null pointer check in case package name was not found
+                        }
+                    }
+                    return true;
                 }
 
                 // Otherwise allow the OS to handle things like tel, mailto, etc.
@@ -52,5 +77,14 @@ public class MainActivity extends AppCompatActivity {
 
         myWebView.loadUrl("https://hellsrevenge.github.io/bcbc-project1/");
 
+    }
+
+    public boolean isPackageInstalled(String packageName, PackageManager packageManager) {
+        try {
+            return packageManager.getApplicationInfo(packageName, 0).enabled;
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 }
